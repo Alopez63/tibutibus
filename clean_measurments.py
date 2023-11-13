4e95f1529b213c579a9ad2551e552c5955dfc669
Adrian Lopez <150659876+Alopez63@users.noreply.github.com>
2023-11-13 14:47:09 +0000

    prefix for log

--- clean_measurments.py
+++ clean_measurments.py
@@ -1,10 +1,18 @@
+import datetime
+
 # Open the measurements log file from bGeigie Nano
 log_file  = 'data/bGeigie_measurements_log.csv'
 gpsdata = open(log_file)
 
 # Open or Create a file to store records that are Ok as GPS Available
-output_file = 'data/clean.log'
-clean_measurments = open(output_file, "w")
+output_dir = 'data'
+output_file = 'clean.log' 
+# creat a prefix to append to the log file name format yyyymmddhhmm
+date_prefix = datetime.datetime.now()
+x = date_prefix.strftime("%Y%m%d%H%M%S")
+y = output_dir+'/'+x+'_'+output_file
+
+clean_measurments = open(y, "w")
 good_entries = 0
 for gpsline in gpsdata:
     # let's print only lines that have a valid GSP Fix. See: https://github.com/Safecast/bGeigieNanoKit/wiki/Operations-Manual#data-log
